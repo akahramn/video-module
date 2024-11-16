dd2e4ea3a1e097500befcb9785fdc804d81f8c03
akahramn <a.kahramnn@gmail.com>
2024-11-16 22:30:48 +0300

    First commit

---
+++ src/main/java/com/abdullahkahraman/video_module/VideoModuleApplication.java
@@ -0,0 +1,13 @@
+package com.abdullahkahraman.video_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdullahkahraman/video_module/VideoModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdullahkahraman.video_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
