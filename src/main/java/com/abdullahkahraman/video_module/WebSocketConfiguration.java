cb17db6c99bd05ca2213694716fed3457b2a2ff4
akahramn <a.kahramnn@gmail.com>
2024-11-20 01:01:14 +0300

    adding web socket config

--- src/main/java/com/abdullahkahraman/video_module/WebSocketConfiguration.java
+++ src/main/java/com/abdullahkahraman/video_module/WebSocketConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfiguration implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler( new WebSocketHandler(), "/socket")
+        registry.addHandler( new SocketHandler(), "/socket")
                 .setAllowedOrigins("*");
     }
 }
